ae2d239ff175502e47456632407db848ccf077f8
Good Boy Ninja <66829812+GoodBoyNinja@users.noreply.github.com>
2024-09-01 13:43:02 +0200

    fix bug where build would be affected by the plugin itself

---
+++ vite.config.ts
@@ -0,0 +1,42 @@
+// vite.config.js
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import path from "path-browserify";
+import importExtendscript from "./lib/main";
+import dts from 'vite-plugin-dts';
+
+
+
+let libName = 'rollup-plugin-import-extendscript';
+export default defineConfig(({ command }) => {
+    return {
+        build: {
+            lib: {
+                // Could also be a dictionary or array of multiple entry points
+                entry: resolve(__dirname, 'lib/main.ts'),
+                name: `${libName}`,
+                formats: ['es'],
+                // the proper extensions will be added
+                fileName: (format) => `${libName}.${format}.js`,
+            },
+            minify: false,
+            rollupOptions: {
+                external: ['fs', 'path', 'fs-extra', 'crypto'],
+            },
+
+        },
+        plugins: [
+            dts({ rollupTypes: true }),
+            command === 'serve' && importExtendscript({ explicit: true }),
+
+
+        ],
+
+        resolve: {
+            alias: {
+                path: "path-browserify",
+            },
+        }
+    }
+});
